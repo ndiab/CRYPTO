586842ce6ebabe41d16ea356d2064573532f8485
DIAB NABIL <3502158@ppti-14-501-08.ufr-info-p6.jussieu.fr>
2016-10-13 15:13:28 +0200

    ajout de Tests

--- Test.py
+++ Test.py
@@ -1,6 +1,7 @@
 import unittest
 from Euclide import *
 from Mint import *
+from RSA import *
 
 
 
@@ -9,16 +10,31 @@ class EuclideTest(unittest.TestCase):
 	
 	def test_euclide_algorithm(self):
 		""" Test if the euclide_algorithm working """
+		print("euclide test ...")
 		a = 654987452
 		b = 165496
 		res = euclide_algorithm(a,b)
 		self.assertEqual({ "PGCD":4 , "U":-8109 , "V":32093182 } , res)
 
+class MintTest(unittest.TestCase):
+	""" Test case used for test the Mint's functions """
+
 	def test_mint_fast_exp(self):
 		""" Test the fast exponentiation of a mint """
+		print("Fast exponentiation test ...")
 		m = Mint(41,527)
 		m.fast_exp(37)
 		self.assertEqual( 113, m.value)
+
+class RSATest(unittest.TestCase):
+	""" Test case used for test the RSA's functions """
+	
+	def test_keys_1(self):
+		""" Test if e * d = 1 mod phi"""
+		print("key test 1 ...")
+		test = RSA()
+		ed = (test.e * test.d)% test.phi
+		self.assertEqual( 1, ed)
 		
 
 if __name__ == '__main__' :
