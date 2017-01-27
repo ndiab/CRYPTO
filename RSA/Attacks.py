94189eb8d2102024017b71461ef432f54d903adf
DIAB NABIL <3502158@ppti-14-502-09.ufr-info-p6.jussieu.fr>
2017-01-27 13:36:39 +0100

    ajout de l attaque de l'exo 2 de l exam 2016

--- RSA/Attacks.py
+++ RSA/Attacks.py
@@ -92,8 +92,55 @@ def Wiener(N : int, e : int) -> int :
 	print("Not vulnerable to Wiener")
 
 	return 0
+
+def attack_exo_2(N : int, e : int, N_bis : int, e_bis : int) -> int :
+	d = Wiener (N,e)
+	nblb = int(log2(d)) + 1  # number of least significant bits k known
+
+	offset = 1 << (nblb +1)	 # Offset for each iteration
+	max_iter = 1 << (276 - nblb)
+
+	print(max_iter)
+	print(offset)
+
+
+	d_bis = offset
+
+	m_ref = randint(10,1000)
+
+	m = Mint(m_ref,N_bis)
+	
+	# brute force the high order bits
+	for i in range (max_iter):
+		# to check if the key works, we compare the encryption and the decryption	
 		
+		if i%100000 == 0 :
+			print (i)
+	 
+		m.fast_exp(e_bis)
+		m.fast_exp(d_bis)
+		
+		if (m.value == m_ref) :
+			print("Hacked")
+			print(d_bis)
+			return d_bis
 
 
-	
-	
+		d_bis = d_bis + offset
+		m.value = m_ref
+
+
+	print("doesn't work")
+	return 0
+
+
+
+N= 0x1000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000143b7ef096124769b084359a72b000000000000000000000000000000000000000000000000000000000000000000000000000002ab354728c9c9e417c3a3017028b5f14ed6cb2a2abccfe41f4bcf
+e= 0x9f4f9e2c3634631d7ce5f6d92316acb180cfed06a759d840735e3f5c230f2f4f1822646d21ea6d523ca1fe48223a74dc2bd8d1b9d722ced486219b3a5f9e1812fc1eeaaa7d11699b576bbf87a2b2684d5555df984e355a4f4a3c392cec236847a2e97d4c6575292744a988f907fec624b9cf1379e145423155c6336992092bf
+
+
+Nbis= 0x1000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000143b7ef096124769b084359a72c620000000000000000000000000000000000000000000000000000000000000000000000000002ab354728c9c9e417c3a301a2bfb5958214c121a20a1bc3a60b5d
+ebis= 0xcab33aef8b29f93115458e31b7f14ff3efbdf91dfaa4e025dafe66913c8db9ab77e860536b79875327449e2c759a774d91c0f09595589613c804f4565dcb202e78559233b609d0a578e9a8029b255ef11179303c1335af7db80ea574488c4195eb259e9dd1b85a9a45eef8a541e4e03a2a4c6a87af63bfd7fb2fe9cf66060f7
+
+attack_exo_2(N, e , Nbis , ebis)		
+
